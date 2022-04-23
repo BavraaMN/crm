2cc6c8a579012bbccc521af55170520b4a8ce173
Мұрат Бауыржан <BAVRAA@GMAIL.COM>
2022-04-24 01:08:46 +0600

    homework ready

---
+++ script/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+
+console.log(document);
+
+const modal__title = document.getElementsByClassName('modal__title');
+console.log(modal__title[0].innerHTML);
+
+const modal__form=document.getElementsByClassName('modal__form');
+console.log(modal__form);
+
+const modal__checkbox = document.getElementsByClassName('modal__checkbox');
+console.log(modal__checkbox);
+
+const modal__input_modal__input_discount = document.getElementsByClassName('modal__input modal__input_discount');
+console.log(modal__input_modal__input_discount);
+
